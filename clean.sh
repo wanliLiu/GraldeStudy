d32348115f1b53d90859716be25e089d63ec6dd7
Sofia4041 <353124127@qq.com>
2018-03-26 23:46:26 +0800

    Gradle插件开发学习

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "开始清楚了哦"
+./gradlew clean
+echo "搞定了"
---
+++ myplugin/src/main/groovy/com/soli/myplugin/PluginImpl.groovy
@@ -0,0 +1,14 @@
+package com.soli.myplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.task("testTask") <<{
+            println("Hello gradle plugin")
+        }
+    }
+}
